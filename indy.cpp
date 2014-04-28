3a391577be2e33b0471c4eac066925585731adef
Tyler Brock <tyler.brock@gmail.com>
2014-04-28 09:23:24 -0400

    holla

--- indy.cpp
+++ indy.cpp
@@ -5,9 +5,7 @@
 #include <vector>
 #include <memory>
 
-const unsigned LE_GCNO_MAGIC = 0x67636E6F; // gcno
-const unsigned LE_GCDA_MAGIC = 0x67636461; // gcda
-const char FILE_SEP = '.';
+static const char FILE_SEP = '.';
 
 using namespace std;
 
@@ -44,42 +42,44 @@ class Record {
 
 class GCovFile {
     public:
-        GCovFile(string file_name) : handle(new ifstream) {
-            handle->open(file_name.c_str(), ios::in|ios::binary);
+        GCovFile(string file_name) : _handle(new ifstream) {
+            _handle->open(file_name.c_str(), ios::in|ios::binary);
 
-            if (handle->is_open()) {
+            if (_handle->is_open()) {
                 cout << "successfully opened " << file_name << endl;
             } else {
                 cout << "unable to open " << file_name << endl;
             }
 
-            handle->read((char*)&magic, sizeof(magic));
-            handle->read((char*)&version, sizeof(version));
-            handle->read((char*)&stamp, sizeof(stamp));
-
-            if (magic == LE_GCNO_MAGIC) {
-                cout << "this is a GCNO file" << endl;
-            } else if (magic == LE_GCDA_MAGIC) {
-                cout << "this is a GCDA file" << endl;
-            } else {
-                cout << "i don't know what this is" << endl;
+            if (check_file_type()) {
+                cout << "file type confirmed" << endl;
             }
 
-            cout << "version major: " << version.major << endl;
-            cout << "minor version: " << version.minor[0] << version.minor[1] << endl;
+            _handle->read((char*)&_magic, sizeof(_magic));
+            _handle->read((char*)&_version, sizeof(_version));
+            _handle->read((char*)&_stamp, sizeof(_stamp));
+
+            cout << "version major: " << _version.major << endl;
+            cout << "minor version: " << _version.minor[0] << _version.minor[1] << endl;
 
             read_record();
         }
+
+        virtual ~GCovFile() {}
+
+    protected:
+        bool check_file_type() { return magic() == _magic; }
+        virtual uint32_t magic() = 0;
 
     private:
         void read_record() {
             cout << "[RECORD] reading a record" << endl;
             RecordHeader record_header;
-            handle->read((char*)&record_header, sizeof(record_header));
+            _handle->read((char*)&record_header, sizeof(record_header));
             cout << "[RECORD] tag is " << record_header.tag << " len is " << record_header.len * 4 << " bytes" << endl;
             char record_buffer[record_header.len * 4];
-            handle->read(record_buffer, record_header.len * 4);
-            records.push_back(Record(record_header, record_buffer));
+            _handle->read(record_buffer, record_header.len * 4);
+            _records.push_back(Record(record_header, record_buffer));
         }
 
         struct Version {
@@ -88,17 +88,29 @@ class GCovFile {
             char major;
         };
 
-        uint32_t magic;
-        Version version;
-        uint32_t stamp;
-        unique_ptr<ifstream> handle;
-        vector<Record> records;
+        uint32_t _magic;
+        Version _version;
+        uint32_t _stamp;
+        unique_ptr<ifstream> _handle;
+        vector<Record> _records;
 };
 
-class GCNOFile {
+class GCNOFile : public GCovFile {
+    public:
+        GCNOFile(string file_name) : GCovFile(file_name) {};
+        ~GCNOFile() {};
+    private:
+        static const unsigned MAGIC = 0x67636E6F; // gcno
+        uint32_t magic() { return MAGIC; }
 };
 
-class GCDAFile {
+class GCDAFile : public GCovFile {
+    public:
+        GCDAFile(string file_name) : GCovFile(file_name) {};
+        ~GCDAFile() {};
+    private:
+        static const unsigned MAGIC = 0x67636461; // gcda
+        uint32_t magic() { return MAGIC; }
 };
 
 int main(int argc, char** argv) {
@@ -113,8 +125,8 @@ int main(int argc, char** argv) {
     string gcno_filename = program_name + FILE_SEP + "gcno";
     string gcda_filename = program_name + FILE_SEP + "gcda";
 
-    GCovFile gcno_file(gcno_filename);
-    GCovFile gcda_file(gcda_filename);
+    GCNOFile gcno_file(gcno_filename);
+    GCDAFile gcda_file(gcda_filename);
 
     return EXIT_SUCCESS;
 }
